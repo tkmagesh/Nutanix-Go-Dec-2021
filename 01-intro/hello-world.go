bfb855ee830ed9c32a5b9efa1126f93abd914d90
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-13 23:36:45 +0530

    hello world

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,16 @@
+/* Package declaration */
+package main
+
+/* Import dependency packages */
+import "fmt"
+
+/* Package level type declarations */
+
+/* Package init function */
+
+/* main function */
+func main() {
+	fmt.Println("Hello World!")
+}
+
+/* other functions */
