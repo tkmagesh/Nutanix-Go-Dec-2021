43124ab40c4f96e1d10607d23e025a719ef0a99a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-15 00:32:25 +0530

    assignment (sorting) - solution

--- 07-interfaces/assignment.go
+++ 07-interfaces/assignment.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Product struct {
 	Id       int
@@ -17,6 +20,22 @@ func (product Product) String() string {
 
 type Products []Product
 
+func (products Products) SortByName() {
+	sort.Sort(ByName{products})
+}
+
+func (products Products) SortByCost() {
+	sort.Sort(ByCost{products})
+}
+
+func (products Products) SortByUnits() {
+	sort.Sort(ByUnits{products})
+}
+
+func (products Products) SortByCategory() {
+	sort.Sort(ByCategory{products})
+}
+
 func main() {
 	products := Products{
 		Product{105, "Pen", 5, 50, "Stationary"},
@@ -27,9 +46,28 @@ func main() {
 		Product{104, "Scribble Pad", 20, 20, "Stationary"},
 	}
 	//fmt.Println(products.FormatProducts())
+	fmt.Println("Initial List")
 	fmt.Println(products)
 
-	/
+	fmt.Println("Sorting [default by id]")
+	sort.Sort(products)
+	fmt.Println(products)
+
+	fmt.Println("Sorting [by name]")
+	sort.Sort(ByName{products})
+	fmt.Println(products)
+
+	fmt.Println("Sorting [by cost]")
+	products.SortByCost()
+	fmt.Println(products)
+
+	fmt.Println("Sorting [by units]")
+	products.SortByUnits()
+	fmt.Println(products)
+
+	fmt.Println("Sorting [by category]")
+	products.SortByCategory()
+	fmt.Println(products)
 }
 
 //implementation of the fmt.Stringer interface
@@ -41,5 +79,54 @@ func (products Products) String() string {
 	return result
 }
 
-//Create the APIs for sorting the products 
-	// by id, name, cost, units, category
+//Create the APIs for sorting the products (USE sort.Sort())
+// by id, name, cost, units, category
+
+/* Interface implementation */
+func (products Products) Len() int {
+	return len(products)
+}
+
+func (products Products) Less(i, j int) bool {
+	return products[i].Id < products[j].Id
+}
+
+func (products Products) Swap(i, j int) {
+	products[i], products[j] = products[j], products[i]
+}
+
+//to sort by name
+type ByName struct {
+	Products
+}
+
+func (byName ByName) Less(i, j int) bool {
+	return byName.Products[i].Name < byName.Products[j].Name
+}
+
+//to sort by cost
+type ByCost struct {
+	Products
+}
+
+func (byCost ByCost) Less(i, j int) bool {
+	return byCost.Products[i].Cost < byCost.Products[j].Cost
+}
+
+//to sort by units
+type ByUnits struct {
+	Products
+}
+
+func (byUnits ByUnits) Less(i, j int) bool {
+	return byUnits.Products[i].Units < byUnits.Products[j].Units
+}
+
+//to sort by category
+type ByCategory struct {
+	Products
+}
+
+func (byCategory ByCategory) Less(i, j int) bool {
+	return byCategory.Products[i].Category < byCategory.Products[j].Category
+}
