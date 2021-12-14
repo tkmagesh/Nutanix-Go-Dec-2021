c5c08a26c23456b63763ddc13f6ed6205e5ac58e
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-14 23:30:45 +0530

    interface composition

--- 07-interfaces/program.go
+++ 07-interfaces/program.go
@@ -13,6 +13,10 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
 type Rectangle struct {
 	Height float64
 	Width  float64
@@ -22,17 +26,23 @@ func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
 func main() {
 	c := Circle{Radius: 10}
-	PrintArea(c)
+	/*
+		PrintArea(c)
+		PrintPerimeter(c)
+	*/
+	PrintShape(c)
 	r := Rectangle{Height: 10, Width: 10}
-	PrintArea(r)
-	shapesWithArea := []ShapeWithArea{
-		c, r,
-	}
-	for _, shape := range shapesWithArea {
-		fmt.Println(shape.Area())
-	}
+	/*
+		PrintArea(r)
+		PrintPerimeter(r)
+	*/
+	PrintShape(r)
 }
 
 type ShapeWithArea interface {
@@ -41,4 +51,29 @@ type ShapeWithArea interface {
 
 func PrintArea(sa ShapeWithArea) {
 	fmt.Printf("Area = %f\n", sa.Area())
+}
+
+type ShapeWithPerimeter interface {
+	Perimeter() float64
+}
+
+func PrintPerimeter(sa ShapeWithPerimeter) {
+	fmt.Printf("Perimeter = %f\n", sa.Perimeter())
+}
+
+/*
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+*/
+
+type Shape interface {
+	ShapeWithArea
+	ShapeWithPerimeter
+}
+
+func PrintShape(s Shape) {
+	PrintArea(s)
+	PrintPerimeter(s)
 }
